e841044db7bdb8a05a5c891a2fd85de9dc8db61a
Dmitry A.K <dakobzev@gmail.com>
2023-05-17 18:08:44 +0300

    Rename percent to Percent

--- JAVAQA_62/DZ_5.1/src/test/java/BonusServiceTest.java
+++ JAVAQA_62/DZ_5.1/src/test/java/BonusServiceTest.java
@@ -50,6 +50,7 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void shouldCalculateForNoneRegisteredAndOverLimit() {
         BonusService service = new BonusService();
