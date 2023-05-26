6dcf4f971914af8698ac22361189176cc780f330
Dmitry A.K <dakobzev@gmail.com>
2023-05-26 21:37:15 +0300

    новое дз

---
+++ JAVAQA_62/DZ_9.1/src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // имя
+    public String patronymic; // отчество
+    public String surname; // фамилия
+    public boolean subscription; // подписка
+    public String passport; // паспорт
+    public String phone; // телефон
+    FormDate birthday = new FormDate();
+}
